b65087f011091edccd496263210b5cc5d6575aa9
jayanth5151 <jayanth5151@gmail.com>
2025-09-28 00:40:48 +0530

    homeresponsive

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,36 @@
-import { Component } from '@angular/core';
-import { RouterModule ,Router} from '@angular/router'; // <-- import RouterModule
+import { Component, AfterViewInit } from '@angular/core';
+import { RouterModule, Router } from '@angular/router'; // <-- import RouterModule
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../auth.service';
 
+
 @Component({
   selector: 'app-home',
-  imports: [CommonModule,RouterModule], // <-- add RouterModule here
+  imports: [CommonModule, RouterModule], // <-- add RouterModule here
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
-constructor(private authService: AuthService, private router: Router) {}
-onLogout() {
-  this.authService.logout();
-  this.router.navigate(['/login']);
-}
+export class HomeComponent implements AfterViewInit {
+  constructor(private authService: AuthService, private router: Router) { }
+  ngAfterViewInit() {
+    const hamburger = document.getElementById('hamburger');
+    const sidebar = document.getElementById('sidebar');
+    const overlay = document.getElementById('overlay');
+
+    if (hamburger && sidebar && overlay) {
+      hamburger.addEventListener('click', () => {
+        sidebar.classList.toggle('show');
+        overlay.classList.toggle('show');
+      });
+
+      overlay.addEventListener('click', () => {
+        sidebar.classList.remove('show');
+        overlay.classList.remove('show');
+      });
+    }
+  }
+  onLogout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
